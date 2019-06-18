33c1939794ba2819826bcf62588e931f276a4345
Noat <turtlej13@gmail.com>
2019-06-18 21:44:14 +0700

    home

---
+++ AutoCodeSignal/src/pack_1/FileUtil.java
@@ -0,0 +1,71 @@
+package pack_1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FileUtil {
+	public static final String PATH_SRC = "D:\\GitHub\\CodeFight\\Answer";
+	public static boolean isFileContainString(String text, String fileName) {
+		try {
+			File file = new File(fileName);
+			System.out.println("read file " + file.getCanonicalPath());
+			try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+				String strLine;
+				// Read lines from the file, returns null when end of stream
+				// is reached
+				while ((strLine = br.readLine()) != null) {
+					if (strLine.contains(text)) {
+						return true;
+					}
+				}
+			}
+			return false;
+		} catch (Exception e) {
+			System.out.println("isFileContainString failed.");
+			return false;
+		}
+	}
+	
+	public static void deleteFile(String fileName) {
+    	try{
+    		File file = new File(fileName);
+    		if(file.delete()){
+    			System.out.println(file.getName() + " is deleted!");
+    		}else{
+    			System.out.println("Delete operation is failed.");
+    		}
+    	} catch(Exception e){
+    		e.printStackTrace();
+    	}
+	}
+	
+	public static void deleteFileWithText(String path, String text, boolean isContain) {
+		try (Stream<Path> walk = Files.walk(Paths.get(path))) {
+
+			List<String> result = walk.filter(Files::isRegularFile)
+					.map(x -> x.toString()).collect(Collectors.toList());
+
+			for (int i = 0; i < result.size(); i++) {
+				if(isFileContainString(text, result.get(i)) == isContain) {
+					deleteFile(result.get(i));
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		deleteFileWithText(PATH_SRC + "\\JS","def ", true);
+	}
+	
+}
--- AutoCodeSignal/src/pack_1/GetFileSource.java
+++ AutoCodeSignal/src/pack_1/GetFileSource.java
@@ -13,17 +13,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class GetFileSource {
-
+	public static final String PATH_SRC = "D:\\GitHub\\CodeFight\\Answer";
 	public static void main(String[] args) {
 		ArrayList<String> urlList = new ArrayList<>();
 		// declaration and instantiation of objects/variables
-    	System.setProperty("webdriver.chrome.driver","E:\\chromedriver.exe");
+    	System.setProperty("webdriver.chrome.driver","D:\\GitHub\\CodeFight\\chromedriver.exe");
 
-		int count = 2;
+		int count = 8;
 		while (count <1000) {
 			WebDriver driver = new ChromeDriver();
 			count ++;
-			System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Tao file Page : " + count );
+			System.out.println("Tao file Page : " + count );
 			Login(driver);
 			CodeFight(driver, urlList,count);
 			driver.close();
@@ -36,13 +36,13 @@ public class GetFileSource {
 	        String baseUrl = "https://app.codesignal.com/tournaments/page/"+ String.valueOf(page);
 
 			try {
-				/// vao tour
+				/// go tour
 				 driver.get(baseUrl);
-			    Thread.currentThread().sleep(5000);
+			    Thread.currentThread();
+				Thread.sleep(5000);
 
 			    WebElement table = driver.findElement(By.tagName("tbody"));
 			    List<WebElement> rows = table.findElements(By.tagName("tr"));
-			    int i=0;
 			    List<String> urlLst = new ArrayList<>();
 			    for (WebElement webElement : rows) {
 			    	String url = webElement.findElement(By.tagName("a")).getAttribute("href");
@@ -51,9 +51,15 @@ public class GetFileSource {
 
 			    for (String string : urlLst) {
 			    	String urlA = string.concat("/A");
-				    String urlB = string.concat("/B");
-				    LoopAB(driver,urlA);
-				    LoopAB(driver,urlB);
+			    	String urlB = string.concat("/B");
+				    String urlC = string.concat("/C");
+				    String urlD = string.concat("/D");
+				    String urlE = string.concat("/E");
+				    LoopAB(driver,urlA, false);
+				    LoopAB(driver,urlB, false);
+				    LoopAB(driver,urlC, true);
+				    LoopAB(driver,urlD, true);
+				    LoopAB(driver,urlE, true);
 
 				}
 
@@ -66,36 +72,46 @@ public class GetFileSource {
 			return sleep;
 	    }
 
-	public static void LoopAB(WebDriver driver,String url) {
+	public static void LoopAB(WebDriver driver,String url, boolean isNotAB) {
 
 	    try {
 			driver.get(url);
 
-	    	Thread.currentThread().sleep(5000);
+	    	Thread.currentThread();
+			Thread.sleep(5000);
 		    String language ="";
 
 	    	WebElement solution = driver.findElement(By.cssSelector("div:nth-child(2) > .vertical-tabs--item"));;
 		    solution.click();
-		    Thread.currentThread().sleep(5000);
+		    Thread.currentThread();
+			Thread.sleep(5000);
 
 		    List<WebElement> solutionRows = driver.findElements(By.cssSelector(".rt-tr-group"));
 		    solutionRows.get(0).click();
 		    for (WebElement detailSolutionRows : solutionRows) {
 		    	List<WebElement> detailSolution = detailSolutionRows.findElements(By.cssSelector(".rt-td"));
-		    	if (language.length() == 0 && ( detailSolution.get(2).getText().contains("Java")||
-		    			detailSolution.get(2).getText().contains("JS")
-		    			|| detailSolution.get(2).getText().contains("Py3")
-		    			|| detailSolution.get(2).getText().contains("Py2")
-		    			|| detailSolution.get(2).getText().contains("C++"))) {
+		    	if (language.length() == 0
+		    			&& (!isNotAB || "300".equals(detailSolution.get(3).getText()))
+		    			&& ( detailSolution.get(2).getText().contains("Py3")
+			    			|| detailSolution.get(2).getText().contains("Py2"))) {
 		    		language = detailSolution.get(2).getText();
+		    		detailSolution.get(2).click();
+			    	Thread.currentThread();
+					Thread.sleep(2000);
+		    		break;
+		    	}
+		    	if (language.length() == 0
+		    			&& (!isNotAB && "300".equals(detailSolution.get(3).getText()))) {
+		    		language = detailSolution.get(2).getText();
+		    		detailSolution.get(2).click();
+			    	Thread.currentThread();
+					Thread.sleep(2000);
 		    		break;
 		    	}
 		    }
-		    if (language.length() > 0) {
-		    	Thread.currentThread().sleep(2000);
+		    	Thread.currentThread();
+				Thread.sleep(2000);
 		    	WriteFile(driver,language);
-		    }
-
 		} catch (Exception e) {
 			// see task
 			//System.out.println("REGISTER Khong tim thay");
@@ -109,13 +125,16 @@ public class GetFileSource {
 
 			try {
 				driver.get(loginUrl);
-		        Thread.currentThread().sleep(2000);
-		        driver.findElement(By.name("username")).sendKeys("toan-nd_fjs");;
-		        driver.findElement(By.name("password")).sendKeys("111111Aa");
-		        Thread.currentThread().sleep(2000);
+		        Thread.currentThread();
+				Thread.sleep(2000);
+		        driver.findElement(By.name("username")).sendKeys("noatnd");;
+		        driver.findElement(By.name("password")).sendKeys("noatnd@1");
+		        Thread.currentThread();
+				Thread.sleep(2000);
 		        driver.findElement(By.cssSelector(".-padding-v-2 > .button--content")).click();;
 		        System.out.println("Login OK");
-		        Thread.currentThread().sleep(2000);
+		        Thread.currentThread();
+				Thread.sleep(2000);
 
 			}
 			catch (Exception ex) {
@@ -138,23 +157,24 @@ public class GetFileSource {
 			if (menthodName.length() > 0) {
 				//menthodName = menthodName.split("(")[0];
 				//menthodName = menthodName.replace("function ", "");
-			     String folderJS ="E:\\DOCUMENT\\CODESIGNAL\\Selenium\\JS\\";
-			     String folderJava = "E:\\DOCUMENT\\CODESIGNAL\\Selenium\\Java\\";
-			     String folderC = "E:\\DOCUMENT\\CODESIGNAL\\Selenium\\C++\\";
-			     String folderPy2 = "E:\\DOCUMENT\\CODESIGNAL\\Selenium\\Py2\\";
-			     String folderPy3 = "E:\\DOCUMENT\\CODESIGNAL\\Selenium\\Py3\\";
-
+			     String folderJS = PATH_SRC + "\\JS\\";
+			     String folderJava = PATH_SRC + "\\Java\\";
+			     String folderC = PATH_SRC + "\\C++\\";
+			     String folderPy2 = PATH_SRC + "\\Py2\\";
+			     String folderPy3 = PATH_SRC + "\\Py3\\";
 
 			     String folder = "";
 			     String content = "";
 
 			     code.sendKeys(Keys.CONTROL + "a");
-		         Thread.currentThread().sleep(2000);
+		         Thread.currentThread();
+				Thread.sleep(2000);
 		         code.sendKeys(Keys.CONTROL + "c");
 
 		         content = (String) Toolkit.getDefaultToolkit()
 		                 .getSystemClipboard().getData(DataFlavor.stringFlavor);
-		         Thread.currentThread().sleep(1000);
+		         Thread.currentThread();
+				Thread.sleep(1000);
 				if(language.equals("JS")) {
 					folder = folderJS;
 				}
@@ -169,6 +189,9 @@ public class GetFileSource {
 				}
 				if(language.equals("C++")) {
 					folder = folderC;
+				}
+				if(language.equals("")) {
+					folder = "";
 				}
 			    createFileSource(folder,menthodName, language, content);
 
@@ -198,6 +221,3 @@ public class GetFileSource {
 		          writer.close();
 		    }
 		}
-
-
-
